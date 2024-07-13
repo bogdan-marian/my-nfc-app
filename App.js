acaa69e952ab75ba51a503dbd121bfb43cb8dc17
bogdan <bogdan.oloeriu@gmail.com>
2024-07-13 12:59:47 +0200

    ubly button

--- App.js
+++ App.js
@@ -1,25 +1,29 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View} from "react-native";
+import { StyleSheet, View } from "react-native";
 
-import Button from './components/Button'; 
+import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
 
 const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
+
+  
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer placeholderImageSource={PlaceholderImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button label="Choose a photo" />
+        <Button theme="primary" label="Choose a photo" />
         <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
