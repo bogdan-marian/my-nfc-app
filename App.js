558afb06655997de611d00f157cf803c29cfae4e
bogdan <bogdan.oloeriu@gmail.com>
2024-07-13 13:57:27 +0200

    image picker

--- App.js
+++ App.js
@@ -3,12 +3,24 @@ import { StyleSheet, View } from "react-native";
 
 import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
+import * as ImagePicker from 'expo-image-picker';
 
 const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
 
-  
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -16,7 +28,7 @@ export default function App() {
         <ImageViewer placeholderImageSource={PlaceholderImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" />
+        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
         <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
